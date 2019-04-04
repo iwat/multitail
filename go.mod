98efec249f2f96ec0a16c3327c1288d8677eff56
Chaiwat Shuetrakoonpaiboon <iwatth@gmail.com>
2019-04-03 23:28:48 -0700

    Working version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/iwat/multitail
+
+require (
+	github.com/hpcloud/tail v1.0.0
+	golang.org/x/sys v0.0.0-20190322080309-f49334f85ddc // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+var tailers = make(map[string]bool)
+
+type event struct {
+	path string
+	line *tail.Line
+}
+
+func main() {
+	config := tail.Config{}
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [file|pattern ...]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.BoolVar(&config.ReOpen, "F", false, "Reopen recreated files")
+	flag.BoolVar(&config.Follow, "f", false, "Continue looking for new lines ")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+	}
+
+	events := make(chan event)
+	waitGroup := sync.WaitGroup{}
+
+	paths := os.Args[1:]
+	createAllTailers(paths, config, events, &waitGroup)
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			createAllTailers(paths, config, events, &waitGroup)
+		}
+	}()
+
+	go func() {
+		lastPath := ""
+		for e := range events {
+			if lastPath != e.path {
+				log.Println("Found new content in", e.path)
+				lastPath = e.path
+			}
+			if e.line.Err != nil {
+				log.Println("Error tailing", e.path, ":", e.line.Err)
+			}
+			fmt.Println(e.line.Text)
+		}
+	}()
+
+	waitGroup.Wait()
+}
+
+func createAllTailers(paths []string, config tail.Config, events chan<- event, waitGroup *sync.WaitGroup) {
+	for _, path := range paths {
+		createTailers(path, config, events, waitGroup)
+	}
+}
+
+func createTailers(pattern string, config tail.Config, events chan<- event, waitGroup *sync.WaitGroup) error {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
+
+	for _, match := range matches {
+		info, err := os.Stat(match)
+		if err != nil {
+			log.Println(match, "stat error:", err)
+			continue
+		}
+
+		if info.IsDir() {
+			continue
+		}
+
+		if _, ok := tailers[match]; ok {
+			continue
+		}
+
+		t, err := tail.TailFile(match, config)
+		if err != nil {
+			log.Println("Error tailing", match, err)
+			continue
+		}
+
+		tailers[match] = true
+		waitGroup.Add(1)
+		go func(path string) {
+			defer waitGroup.Done()
+			for line := range t.Lines {
+				events <- event{path, line}
+			}
+		}(match)
+	}
+
+	return nil
+}
